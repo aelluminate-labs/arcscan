6eb94f4589a9b49d86e654911ca08637044d5cd5
noeyislearning <ignacio.francis@aelluminate.com>
2024-11-23 21:23:11 +0800

    refactor: remove unused scraping code and modules

--- src/utils/extract_text.py
+++
@@ -1,4 +0,0 @@
-def extract_text(tag, class_name=None):
-    # :: Extract and return text from a tag if it exists.
-    element = tag.find(class_name=class_name) if class_name else tag
-    return element.text.strip() if element else None
